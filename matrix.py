ee7edc7115afeccb39c4eb52e50e4bb637ab51c3
UAlbertaSu <tzuhang@ualberta.ca>
2025-07-20 23:03:20 -0600

    access excel sheet and cells

--- matrix.py
+++ matrix.py
@@ -1,24 +1,27 @@
 import openpyxl
 from openpyxl import load_workbook
 
-def processSheet(wb):
+def processSheet(wb, dict):
     # parse the student ID, city and their address in dictionary
     
     sheet = wb.active
-    for row in sheet.iter_rows(values_only = True):
-        print(row)
+    
+
+    for row_num, row in enumerate(sheet.iter_rows(values_only = True), start = 1):
+        if row_num == 1:
+            pass
 
     
 def main():
     studentWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Students\\ModifiedClassList.xlsx")
     hostListWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Locations\\ModifiedHostList.xlsx")
-
+    studentDict = {}
+    hostDict = {}
     # parse the origins (student address) 
     print("Student Location \n")
-    processSheet(studentWb)
+    processSheet(studentWb, studentDict)
     print("Host Location \n")
-
-    processSheet(hostListWb)
+    processSheet(hostListWb, hostDict)
 
 
 
