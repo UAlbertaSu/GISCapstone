fc385c648e5ee9d9300e39e3587d9b3a43124688
UAlbertaSu <77360262+UAlbertaSu@users.noreply.github.com>
2025-07-22 12:36:48 -0600

    Hide api key, ensured API works

--- matrix.py
+++ matrix.py
@@ -1,28 +1,115 @@
 import openpyxl
 from openpyxl import load_workbook
+import sys # Delete in final
+import requests
+from datetime import datetime, timedelta
+import time
+import json
+import os
+from dotenv import load_dotenv
+
+def get_next_monday_8am():
+    now = datetime.now()
+    tomorrow_8am = (now + timedelta(days=1)).replace(
+        hour=8, minute=0, second=0, microsecond=0
+    )
+    return int(time.mktime(tomorrow_8am.timetuple()))
+
+def get_distance(origin, destination):
+    
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+
+    # API Config
+    
+    url = "https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix"
+
+    # Request Headers
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": api_key,
+        "X-Goog-FieldMask": "originIndex,destinationIndex,duration,distanceMeters,status"
+    }
+
+    # Request Body with Variables
+    payload = {
+        "origins": [{
+            "waypoint": {
+                "address": origin  # Using origin variable
+            }
+        }],
+        "destinations": [{
+            "waypoint": {
+                "address": destination  # Using destination variable
+            }
+        }],
+        "travelMode": "TRANSIT",
+        "transitPreferences": {
+            "routingPreference": "LESS_WALKING",
+            "allowedTravelModes": ["BUS", "SUBWAY", "TRAIN"]
+        },
+        # "departureTime": get_next_monday_8am()
+    }
+
+    # Send Request
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(response.json())
 
 def processSheet(wb, dict):
     # parse the student ID, city and their address in dictionary
     
     sheet = wb.active
     
-
+    
     for row_num, row in enumerate(sheet.iter_rows(values_only = True), start = 1):
         if row_num == 1:
-            pass
-
+            field = row
+        else:
+            if field[0] == "Student ID#":
+                dict[row[0]] = row[1:3]
+                
+            else:
+                hostList = list(row[:2])
+                hostList.append(row[5])
+                hostList.append(row[8])
+                dict[row[3]] = hostList
+    
+    return dict
     
 def main():
-    studentWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Students\\ModifiedClassList.xlsx")
-    hostListWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Locations\\ModifiedHostList.xlsx")
+    # Delete this check in MVP
+    if sys.platform == "win32":
+        studentWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Students\\ModifiedClassList.xlsx")
+        hostListWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Locations\\ModifiedHostList.xlsx")
+    else:
+        studentWb = load_workbook("/Users/stevesu/GIS School/GISCapstone/ModifiedClassList.xlsx")
+        hostListWb = load_workbook("/Users/stevesu/GIS School/GISCapstone/ModifiedHost.xlsx")
+        
     studentDict = {}
     hostDict = {}
     # parse the origins (student address) 
     print("Student Location \n")
-    processSheet(studentWb, studentDict)
+    studentDict = processSheet(studentWb, studentDict)
+    print(studentDict)
+    print('\n')
     print("Host Location \n")
-    processSheet(hostListWb, hostDict)
+    # parse the destination and attributes
+    hostDict = processSheet(hostListWb, hostDict)
+    print(hostDict)
+    print('\n')
+    for k,v in studentDict.items():
+        origin = (v[0]+" "+ v[1])
+        for key, value in hostDict.items():
+            destination = (key+" "+value[2])
+            print(origin, destination)
+            get_distance(origin, destination)
+            
+            
+ 
 
+
+
+    
 
 
 
---
+++ test.py
@@ -0,0 +1,3 @@
+import sys
+
+print("OS platform:", sys.platform)
