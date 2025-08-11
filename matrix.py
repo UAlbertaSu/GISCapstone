37c60e4b1d6ae74300960d37b9fb7dbe6751c1f6
UAlbertaSu <tzuhang@ualberta.ca>
2025-08-10 22:32:00 -0600

    Excel output Completed

--- matrix.py
+++ matrix.py
@@ -1,12 +1,13 @@
-import openpyxl
 from openpyxl import load_workbook
-import sys # Delete in final
+from openpyxl import Workbook
+from openpyxl.styles import Font
 import requests
 from datetime import datetime, timedelta, timezone
 import time
 import json
 import os
 import urllib.parse 
+
 from dotenv import load_dotenv
 
 
@@ -48,7 +49,6 @@ def get_distance(origin, destination):
         } for address in destination],
         "travelMode": "TRANSIT",
         "transitPreferences": {
-            # "routingPreference": "LESS_WALKING",
             "allowedTravelModes": ["BUS", "RAIL"]
         },
         "departureTime": get_next_monday_8am()
@@ -82,31 +82,35 @@ def processSheet(wb, dict):
     
 def main():
     # Delete this check in MVP
-    if sys.platform == "win32":
-        studentWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Students\\ModifiedClassList.xlsx")
-        hostListWb = load_workbook("C:\\Users\steve\SAIT\Sem2\\Capstone\\Inputs\Locations\\ModifiedHostList.xlsx")
-    else:
-        studentWb = load_workbook("/Users/stevesu/GIS School/GISCapstone/ModifiedClassList.xlsx")
-        hostListWb = load_workbook("/Users/stevesu/GIS School/GISCapstone/ModifiedHost.xlsx")
-        
+     
+    timeStamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    while True:
+        try:
+            studentFilePath = input("Please enter the absolute path of the Classlist excel sheet: ")
+            practicumLocationPath = input("Please enter the absolute folder path of the host location excel sheet: ")
+            outputPath = input("Please give the folder location of where you want to save the output excel spreadsheet: ")
+            os.makedirs(outputPath, exist_ok=True)
+
+            studentWb = load_workbook(studentFilePath)
+            hostListWb = load_workbook(practicumLocationPath)
+            break
+        except:
+            print("Please make sure the path is correct and includes the file name and extension. (.xlsx) where applicable")
+            
     studentDict = {}
     hostDict = {}
     
     # parse the origins (student address) 
     print("Student Location \n")
     studentDict = processSheet(studentWb, studentDict)
-    # print(studentDict)
-    # print('\n')
-    # print("Host Location \n")
-    
+  
     # parse the destination and attributes
     hostDict = processSheet(hostListWb, hostDict)
-    # print(hostDict)
-    # print('\n')
     travelTimeMatrix = {}
 
     # call API
     for k,v in studentDict.items():
+        #combine the City name with the address to specify search parameter for google maps
         origin = (v[0]+" "+ v[1])
         travelTimeMatrix[k] = {}
         studentCity = origin.strip().split()[-1]
@@ -121,6 +125,7 @@ def main():
             if studentCity.lower() != hostCity.lower():
                 continue
             # add additional checks here to trim calls
+            # for example student - host mentorship criteria
             
             hostDestination.append(destination)
             hostKeys.append(key)
@@ -147,18 +152,57 @@ def main():
                 timeToTravel.append({destinationKey: shortest_duration})
             
             travelTimeMatrix[k] = timeToTravel
+    
     print(travelTimeMatrix)
     # print it out nicely and create a maps url 
+    # Put result in Excel. 
+    # r = results
+    # rs = resultSheet
+    r = Workbook()
+    rs = r.active
+    rs.title = "BVCOutput"
+    
+    headers = ["Student ID", "Origin", "Destination", "Duration in minutes", "Google Maps"]
+    for col, header in enumerate(headers, 1):
+        cell = rs.cell(row=1, column = col, value = header)
+        cell.font = Font(b = True)
+
+    row_num = 2
+       
     for student, locations in travelTimeMatrix.items():
         print(f"student ID : {student}, Origin: {studentDict[student][0]}, {studentDict[student][1]}")
+        rs.cell(row = row_num, column = 1, value = student)
+        rs.cell(row = row_num, column= 2, value = studentDict[student][0]+","+ studentDict[student][1])
         studentAddress =  urllib.parse.urlencode({"origin" : studentDict[student][0]+","+studentDict[student][1]})
         for location in locations:
             for address, duration in location.items():
                 destinationAddress =urllib.parse.urlencode({"destination" : {address.strip()} })
                 url = "https://www.google.com/maps/dir/?api=1&"+studentAddress+ "&"+ destinationAddress+"&travelmode=transit"
                 print(f" Address: {address.strip()} | {duration} seconds | {url}")
-                
-    # Put result in Excel. 
+                rs.cell(row= row_num, column=3, value = address.strip())
+                rs.cell(row= row_num, column=4, value = duration/60)
+                rs.cell(row= row_num, column=5, value = url)
+                row_num += 1
+    
+    # format the cells width
+    for column in rs.columns:
+        max_length = 0
+        column_letter = column[0].column_letter
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(str(cell.value))
+            except:
+                pass
+        adjusted_width = (max_length + 2)
+        rs.column_dimensions[column_letter].width = adjusted_width
+    #Generate output excel 
+    filename = f"BVCoutput_{timeStamp}.xlsx"
+    r.save(f"{outputPath}\{filename}")
+
+    print(f"Output created in {outputPath}\{filename}")
 main()
 
 
+# C:\Users\steve\SAIT\Sem2\Capstone\Inputs\Students\ModifiedClassList.xlsx
+# C:\Users\steve\SAIT\Sem2\Capstone\Inputs\Locations\ModifiedHostList.xlsx
